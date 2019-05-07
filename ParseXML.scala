2352bd8ecbd1a8c86ffbf09ab5c83874a559a0d2
David Martuscello <dm4350@nyu.edu>
2019-05-07 11:36:40 -0400

    experiment with smaller sites

--- ParseXML.scala
+++ ParseXML.scala
@@ -1,11 +1,11 @@
 
-val siteList = Seq("fitness.stackexchange.com.7z", "health.stackexchange.com.7z","biology.stackexchange.com.7z","graphicdesign.stackexchange.com.7z")
+val siteList = Seq("languagelearning.stackexchange.com.7z", "alcohol.stackexchange.com.7z","coffee.stackexchange.com.7z","medicalsciences.stackexchange.com.7z")
 
 val firstRDD = getTextDF(siteList(0))
 val secondRDD = getTextDF(siteList(1))
-//val thirdRDD = getTextDF(siteList(2))
+val thirdRDD = getTextDF(siteList(2))
 //val fourthRDD = getTextDF(siteList(3))
-val combinedRDD = firstRDD.union(secondRDD)//.union(thirdRDD).union(fourthRDD)
+val combinedRDD = firstRDD.union(secondRDD).union(thirdRDD)//.union(fourthRDD)
 val reducedRDD = combinedRDD.reduceByKey((a: String,b: String) => a + b)
 reducedRDD.take(1)
 
